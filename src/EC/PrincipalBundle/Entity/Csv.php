09054f420bcb9a1df44fae255f67eb4258df5c97
rafa <rasamu24@hotmail.com>
2014-05-21 21:00:45 +0200

    8-Revision: Accesibilidad,validacion

---
+++ src/EC/PrincipalBundle/Entity/Csv.php
@@ -0,0 +1,44 @@
+<?php
+namespace EC\PrincipalBundle\Entity;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * EC\PrincipalBundle\Entity\Csv
+ */
+class Csv
+{
+    /**
+     * @Assert\NotBlank
+     * @Assert\File(
+     *	maxSize = "100k",
+     *	maxSizeMessage = "El fichero ocupa demasiado.",
+     *	notFoundMessage = "Seleccione un fichero.",
+     *   mimeTypes = {"text/plain", "text/plain"},
+     *   mimeTypesMessage = "Por favor, suba un fichero CSV válido."
+     * )
+     */
+    private $file;
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+}
+
